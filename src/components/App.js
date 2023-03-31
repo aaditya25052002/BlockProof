d6f5aa55449f1835735d72c1724f69cbcc1d3d76
dixitt5 <tilajidixit@gmail.com>
2023-03-31 11:27:55 +0530

    major changes in front-end

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React, { Component } from 'react';
-import Web3 from 'web3';
-import Dvault from '../abis/blockProof.json'; 
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import { HalfCircleSpinner } from 'react-epic-spinners';
-import Home from './Home'
-import Issue from './Issue'
-import Certificate from './Certificate'
-import Navbar from './Navbar'
-
+import React, { Component } from "react";
+import Web3 from "web3";
+import Dvault from "../abis/blockProof.json";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HalfCircleSpinner } from "react-epic-spinners";
+import Home from "./Home";
+import Issue from "./Issue";
+import Certificate from "./Certificate";
+import Navbar from "./Navbar";
+import Login from "./Login";
 
 class App extends Component {
-
   async componentWillMount() {
-    await this.loadWeb3()
-    await this.loadBlockchainData()
+    await this.loadWeb3();
+    await this.loadBlockchainData();
   }
 
   async loadWeb3() {
@@ -31,97 +30,106 @@ class App extends Component {
   }
 
   async loadBlockchainData() {
-    const web3 = window.web3
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const networkId = await web3.eth.net.getId()
-    const networkData = Dvault.networks[networkId]
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    this.setState({ account: accounts[0] });
+    const networkId = await web3.eth.net.getId();
+    const networkData = Dvault.networks[networkId];
 
     if (networkData) {
-      const dvault = new web3.eth.Contract(Dvault.abi, networkData.address)
-      this.setState({ dvault })
-      const uCount = await dvault.methods.uCount.call()
+      const dvault = new web3.eth.Contract(Dvault.abi, networkData.address);
+      this.setState({ dvault });
+      const uCount = await dvault.methods.uCount.call();
       for (let i = 1; i <= uCount; i++) {
-        const user = await dvault.methods.users(i).call()
+        const user = await dvault.methods.users(i).call();
         this.setState({
-            users: [...this.state.users, user]
-        })
+          users: [...this.state.users, user],
+        });
       }
 
-      const cCount = await dvault.methods.cCount.call()
+      const cCount = await dvault.methods.cCount.call();
       for (let j = 1; j <= cCount; j++) {
-        console.log(cCount.toString())
-        const certificate = await dvault.methods.certificates(j).call()
-        
+        console.log(cCount.toString());
+        const certificate = await dvault.methods.certificates(j).call();
+
         this.setState({
-          certificates: [...this.state.certificates, certificate]
-        })
-        if ((certificate.issuer === this.state.account || certificate.recipient === this.state.account) && (certificate.isValid)) {
+          certificates: [...this.state.certificates, certificate],
+        });
+        if (
+          (certificate.issuer === this.state.account ||
+            certificate.recipient === this.state.account) &&
+          certificate.isValid
+        ) {
           this.setState({
-            myCertificates: [...this.state.myCertificates, certificate]
-          })
+            myCertificates: [...this.state.myCertificates, certificate],
+          });
         }
       }
-      
-       this.setState({ loading: false })
+
+      this.setState({ loading: false });
     } else {
-      window.alert('Contract could not be deployed.')
+      window.alert("Contract could not be deployed.");
     }
-    console.log("hi")
+    console.log("hi");
   }
 
   createUser(name, type) {
-    this.setState({ loading: true })
-    this.state.dvault.methods.createUser(name, type).send({ from: this.state.account })
-    .once('confirmation', (n, receipt) => {
-      this.setState({ loading: false })
-      window.location.reload()
-    })
+    this.setState({ loading: true });
+    this.state.dvault.methods
+      .createUser(name, type)
+      .send({ from: this.state.account })
+      .once("confirmation", (n, receipt) => {
+        this.setState({ loading: false });
+        window.location.reload();
+      });
   }
 
   issueCertificate(url, recipient, desc) {
-    this.setState({ loading: true })
-    this.state.dvault.methods.issueCertificate(url, recipient, desc).send({ from: this.state.account })
-    .once('confirmation', (n, receipt) => {
-      window.alert("Certificate successfully issued")
+    this.setState({ loading: true });
+    this.state.dvault.methods
+      .issueCertificate(url, recipient, desc)
+      .send({ from: this.state.account })
+      .once("confirmation", (n, receipt) => {
+        window.alert("Certificate successfully issued");
 
-      this.setState({ loading: false })
-      window.location.reload()
-    })
+        this.setState({ loading: false });
+        window.location.reload();
+      });
   }
 
   revokeCertificate(id) {
-    this.setState({ loading: true })
-    this.state.dvault.methods.revokeCertificate(id).send({ from: this.state.account })
-    .once('confirmation', (n, receipt) => {
-      this.setState({ loading: false })
-      window.location.reload()
-    })
+    this.setState({ loading: true });
+    this.state.dvault.methods
+      .revokeCertificate(id)
+      .send({ from: this.state.account })
+      .once("confirmation", (n, receipt) => {
+        this.setState({ loading: false });
+        window.location.reload();
+      });
   }
 
   async verifyCertificate(uid) {
-    this.setState({ loading: true })
-    const res = await this.state.dvault.methods.verifyCertificate(uid).call()
-    this.setState({ loading: false })
-    return res
+    this.setState({ loading: true });
+    const res = await this.state.dvault.methods.verifyCertificate(uid).call();
+    this.setState({ loading: false });
+    return res;
   }
 
-
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      account: '',
+      account: "",
       dvault: null,
       users: [],
       certificates: [],
       myCertificates: [],
-      loading: false
-    }
+      loading: false,
+    };
 
-    this.createUser = this.createUser.bind(this)
-    this.issueCertificate = this.issueCertificate.bind(this)
-    this.revokeCertificate = this.revokeCertificate.bind(this)
-    this.verifyCertificate = this.verifyCertificate.bind(this)
+    this.createUser = this.createUser.bind(this);
+    this.issueCertificate = this.issueCertificate.bind(this);
+    this.revokeCertificate = this.revokeCertificate.bind(this);
+    this.verifyCertificate = this.verifyCertificate.bind(this);
   }
 
   render() {
@@ -129,36 +137,69 @@ class App extends Component {
       <div style={{ height: 800 }}>
         <Router>
           <Navbar />
-          <Route exact path="/" render={props => (
-            <React.Fragment>
-              {
-                this.state.loading
-                  ? <div class="center"><HalfCircleSpinner size="100" color="red" /></div>
-                  : <Home account={this.state.account} certificates={this.state.certificates} verifyCertificate={this.verifyCertificate} />
-              }
-            </React.Fragment>
-          )} />
+          <Route
+            exact
+            path="/main"
+            render={(props) => (
+              <React.Fragment>
+                {this.state.loading ? (
+                  <div class="center">
+                    <HalfCircleSpinner size="100" color="black" />
+                  </div>
+                ) : (
+                  <Home
+                    account={this.state.account}
+                    certificates={this.state.certificates}
+                    verifyCertificate={this.verifyCertificate}
+                  />
+                )}
+              </React.Fragment>
+            )}
+          />
 
-          <Route exact path="/issue" render={props => (
+          <Route
+            exact
+            path="/issue"
+            render={(props) => (
               <React.Fragment>
-                {
-                  this.state.loading
-                    ? <div class="center"><HalfCircleSpinner size="100" color="red" /></div>
-                    : <Issue users={this.state.users} account={this.state.account} createUser={this.createUser} issueCertificate={this.issueCertificate} />
-                }
+                {this.state.loading ? (
+                  <div class="center">
+                    <HalfCircleSpinner size="100" color="black" />
+                  </div>
+                ) : (
+                  <Issue
+                    users={this.state.users}
+                    account={this.state.account}
+                    createUser={this.createUser}
+                    issueCertificate={this.issueCertificate}
+                  />
+                )}
               </React.Fragment>
-          )} />
-   
-          <Route exact path="/certificates" render={props => (
+            )}
+          />
+
+          <Route
+            exact
+            path="/certificates"
+            render={(props) => (
               <React.Fragment>
-                {
-                  this.state.loading
-                    ? <div class="center"><HalfCircleSpinner size="100" color="red" /></div>
-                    : <Certificate users={this.state.users} account={this.state.account} myCertificates={this.state.myCertificates} revokeCertificate={this.revokeCertificate} />
-                }
+                {this.state.loading ? (
+                  <div class="center">
+                    <HalfCircleSpinner size="100" color="black" />
+                  </div>
+                ) : (
+                  <Certificate
+                    users={this.state.users}
+                    account={this.state.account}
+                    myCertificates={this.state.myCertificates}
+                    revokeCertificate={this.revokeCertificate}
+                  />
+                )}
               </React.Fragment>
-          )} />
-          </Router>
+            )}
+          />
+          <Route exact path="/" render={() => <Login />} />
+        </Router>
       </div>
     );
   }
---
+++ src/components/Banner.js
@@ -0,0 +1,120 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+const Banner = () => {
+  return (
+    <>
+      <section
+        id="header"
+        className="d-flex align-items-center  not-found-container"
+        style={{ marginTop: "50px" }}
+      >
+        <div className="container-fluid nav_bg">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center animate__animated animate__fadeInLeft">
+                  {/* //Your dream websites and instant apps awaits ! */}
+                  <h2>Why Blockchain, why us?</h2>
+                  <h9>
+                    Blockchain technology has revolutionized the way we verify
+                    and create certificates. Its unique features make it an
+                    ideal solution for this purpose. One of the key benefits of
+                    blockchain is its immutability. Once information is added to
+                    the blockchain, it cannot be altered or deleted, ensuring
+                    the integrity of the information contained in certificates.
+                    Additionally, blockchain is a decentralized system, meaning
+                    that there is no central authority controlling it, making
+                    certificates more reliable and trustworthy. The transparency
+                    and security features of blockchain also make it an
+                    attractive option for certificate verification. All
+                    participants in the network can see the transactions and
+                    certificates that have been added to the ledger, ensuring
+                    their authenticity.
+                  </h9>
+                  <div className="mt-3">
+                    <Link to="/issue">
+                      <Button variant="success">Generate certificate</Button>{" "}
+                    </Link>
+                    <Link to="/certificates">
+                      {" "}
+                      <Button variant="info">History</Button>{" "}
+                    </Link>
+                  </div>
+                </div>
+
+                <div className="col-md-6 order-1 order-lg-2 header-img animate__animated animate__fadeInUp">
+                  <img
+                    src="https://i.gifer.com/7sYl.gif"
+                    // src="https://i.gifer.com/1j64.gif"
+                    className="img-fluid-animated"
+                    alt="homeimg"
+                    style={{
+                      height: "300px",
+                      marginLeft: "150px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section
+        id="header"
+        className="d-flex align-items-center  not-found-container"
+        style={{ marginTop: "100px" }}
+      >
+        <div className="container-fluid nav_bg">
+          <div className="row">
+            <div className="col-10 mx-auto">
+              <div className="row">
+                <div className="col-md-6 pt-5 pt-lg-0 order-1 order-lg-2 d-flex flex-column justify-content-center animate__animated animate__fadeInLeft">
+                  {/* //Your dream websites and instant apps awaits ! */}
+                  <h3>zkEVM compatibility </h3>
+                  <h9>
+                    Zero-knowledge proofs (ZKPs) on Polygon offer a powerful
+                    tool for verifying the authenticity of certificates while
+                    preserving privacy. By leveraging ZKPs, a prover can
+                    generate a proof of a statement's truth without revealing
+                    any additional information beyond the statement. In the
+                    context of certificates, this means that sensitive
+                    information such as grades, exam scores, or personal data
+                    can remain confidential while the certificate's authenticity
+                    can be verified. This approach can be beneficial in
+                    scenarios such as academic degrees or professional
+                    certifications, where both parties need assurance of
+                    authenticity without revealing sensitive information to each
+                    other.
+                  </h9>
+                  <div className="mt-3">
+                    <Button variant="success">Documentation</Button>{" "}
+                  </div>
+                </div>
+
+                <div className="col-md-6 order-2 order-lg-1 header-img animate__animated animate__fadeInUp">
+                  <img
+                    // src="https://i.gifer.com/7sYl.gif"
+                    src="https://i.gifer.com/1j64.gif"
+                    className="img-fluid-animated"
+                    alt="homeimg"
+                    style={{
+                      height: "300px",
+                      marginRight: "150px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <br />
+      <br />
+      <br />
+    </>
+  );
+};
+export default Banner;
